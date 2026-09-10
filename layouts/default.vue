caed913a998418a63316ec6b2b0904e04582aa32
Jonas Möller <jonas.moeller23@icloud.com>
2026-09-10 16:19:54 +0200

    added: default design

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import Schwelle from "../components/schwelle.vue";
+</script>
+<slot/>
+<template>
+  <div id="main">
+    <slot/>
+    <schwelle color="var(--db-red)" number-of-lines="12" height="50px" id="schwelle"/>
+  </div>
+</template>
+
+<style scoped>
+#main {
+  padding: 32px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+
+#schwelle {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+
+</style>
